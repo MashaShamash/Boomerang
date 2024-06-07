c99f61687ae0c35d8a234ad9afbde1d16876e75d
MashaShamash <mariapanasenko26@yandex.ru>
2024-06-07 10:54:30 +0300

    first commit

---
+++ src/View.js
@@ -0,0 +1,16 @@
+// Сделаем отдельный класс для отображения игры в консоли.
+
+class View {
+  render(track) {
+    const yourTeamName = 'Elbrus';
+
+    // Тут всё рисуем.
+    console.clear();
+
+    console.log(track.join(''));
+    console.log('\n\n');
+    console.log(`Created by "${yourTeamName}" with love`);
+  }
+}
+
+module.exports = View;
